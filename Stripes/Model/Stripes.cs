bb63cfaac415ff30d353236d5a6ffe009b83b62a
MaxAlex <96299185+Mastetytu@users.noreply.github.com>
2024-03-11 23:58:33 +0300

    m

---
+++ Stripes/Model/Stripes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Stripes.Model
+{
+    public class Stripes
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Colors Colors { get; set; }
+    }
+}
